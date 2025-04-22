7f423bfbab7b82ef94d46f6d17c5283c65325f05
AlPogorelov <eczempl@gmail.com>
2025-04-22 20:20:38 +0600

    Done

--- habit/tests.py
+++ habit/tests.py
@@ -77,7 +77,6 @@ class HabitViewSetTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
     def test_habit_update_owner_permission(self):
         self.client.force_authenticate(user=self.other_user)
         url = reverse('habit:habits-detail', args=[self.habit.id])
